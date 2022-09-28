a1ae54068162df790c5426fc05b1d3238199844c
gay <mraends@gmail.com>
2022-09-28 18:58:53 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,9 @@
+document.querySelector('.censor').addEventListener('click', function(lol){
+    document.querySelector(' .l ').classList.toggle('active')
+    document.querySelector('.r ').classList.toggle('active')
+    document.querySelector('.censor ').classList.toggle('activee')
+})
+document.querySelector('.l').addEventListener('click', function(lol){
+    
+    document.querySelector('.censor ').classList.toggle('activee')
+})
